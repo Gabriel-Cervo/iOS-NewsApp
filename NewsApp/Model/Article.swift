34687eaf77f45eb2c93e94f792cc09404702a5db
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-07-11 00:43:34 -0300

    Added article model

---
+++ NewsApp/Model/Article.swift
@@ -0,0 +1,24 @@
+//
+//  Article.swift
+//  NewsApp
+//
+//  Created by Joao Gabriel Dourado Cervo on 11/07/21.
+//
+
+import Foundation
+
+struct Article: Decodable {
+    let source: ArticleSource
+    let author: String
+    let title: String
+    let description: String
+    let url: String
+    let urlToImage: String
+    let publishedAt: String
+    var publishedDate: Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale.current
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ" // Api response example -> 2021-07-11T03:00:00Z
+        return dateFormatter.date(from: publishedAt) ?? Date()
+    }
+}
---
+++ NewsApp/Model/ArticleSource.swift
@@ -0,0 +1,12 @@
+//
+//  ArticleSource.swift
+//  NewsApp
+//
+//  Created by Joao Gabriel Dourado Cervo on 11/07/21.
+//
+
+import Foundation
+
+struct ArticleSource: Decodable {
+    var name: String
+}
