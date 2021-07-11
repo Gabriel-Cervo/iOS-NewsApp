586ebb5d264218c90e92281e824251b2da851340
Gabriel-Cervo <joaogabrieldouradocervo@gmail.com>
2021-07-10 23:58:55 -0300

    Initial commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Joao Gabriel Dourado Cervo on 10/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
